5fd7c83b02645420f25f32fba0341a3c4216ac32
Moises Fernandes de Paula <you@example.com>
2022-03-16 15:55:54 -0300

    redirection between pages

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  signUpUser(){
+    this.router.navigate(['login']);
   }
 
 }
